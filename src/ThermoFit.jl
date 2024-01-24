5b656b5cca2650b65421a6285cd8eb3cf9d7aa8f
Pierre Lanari <pierre.lanari@unibe.ch>
2024-01-24 16:57:31 +0100

    Update ThermoFit.jl

--- src/ThermoFit.jl
+++ src/ThermoFit.jl
@@ -8,23 +8,16 @@ using MAGEMin_jll
 @reexport using MAGEMin_C
 
 include("params.jl")
+include("utils.jl")
 include("bingo.jl")
 include("forward.jl")
 include("inversion.jl")
-include("utils.jl")
 
 
-
-
-
+export global_constants, global_params
+export utils, load_bulk, calc_structural_formula_element_from_output
 export bingo, bingo_calculate_qcmp_phase, bingo_generate_fake_uncertainties
 export forward, callMAGEMin
-export Constraint, objective_function
-export global_constants, global_params
-export load_bulk
-export utils, calc_structural_formula_element_from_output
-
-
-
+export inversion, constraint, objective_function
 
 end

0502dd8157576d986555bd775a10a7d9513be705
philip <philip.hartmeier@hotmail.com>
2024-01-27 09:59:12 +0100

    Overwrite Margules

--- test/multithread_test.jl
+++ test/multithread_test.jl
@@ -10,6 +10,9 @@ sys_in = "wt"
 pressures = [8.0, 8.0]
 temperatures = [650, 600]
 
+phase = "bi"
+w_g = [12.0, 4.0, 10.0, 30.0, 8.0, 9.0, 8.0, 15.0, 32.0, 13.6, 6.3, 7.0, 24.0, 5.6, 8.1, 40.0, 1.0, 13.0, 40.0, 30.0, 11.6]
+
 let id      = 1
     gv          = MAGEMin_db.gv[id]
     z_b         = MAGEMin_db.z_b[id]
@@ -22,6 +25,7 @@ end
 
 @threads :static for i in eachindex(pressures)
     id          = Threads.threadid()
+
     MAGEMin_db.gv[id] = define_bulk_rock(MAGEMin_db.gv[i], bulk, bulk_oxides, sys_in, MAGEMin_db.db)
 
     gv          = MAGEMin_db.gv[id]
@@ -29,11 +33,17 @@ end
     DB          = MAGEMin_db.DB[id]
     splx_data   = MAGEMin_db.splx_data[id]
 
+    gv, z_b, DB, splx_data = pwm_init(pressures[i], temperatures[i], gv, z_b, DB, splx_data)
 
-    out = point_wise_minimization(pressures[i], temperatures[i], gv, z_b, DB, splx_data)
+    ss_names  = unsafe_string.(unsafe_wrap(Vector{Ptr{Int8}}, gv.SS_list, gv.len_ss));
+    ss_struct = unsafe_wrap(Vector{LibMAGEMin.SS_ref},DB.SS_ref_db,gv.len_ss);
 
-    # gv, z_b, DB, splx_data = pwm_init(pressure, temperature, gv, z_b, DB, splx_data);
-    # out = pwm_run(gv, z_b, DB, splx_data);
+    ss_idx = findfirst(x->x==phase, ss_names);
+
+    W = unsafe_wrap(Vector{Cdouble},ss_struct[ss_idx].W, ss_struct[ss_idx].n_w);
+    W[:] = w_g
+
+    out = pwm_run(gv, z_b, DB, splx_data);
 end
 
 # finalize_MAGEMin(gv,DB, z_b)

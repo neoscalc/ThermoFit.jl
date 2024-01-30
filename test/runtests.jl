26f8b5d79df93e0ef2a5be2be86801dc05f27e22
philip <philip.hartmeier@hotmail.com>
2024-01-30 12:55:23 +0100

    Inversion tests added. Including a small test dataset for inversion constraints.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,142 @@ using Test
 CST = global_constants()
 PARAMS = global_parameters()
 
+@testset "ThermoFit" begin
+    cd(@__DIR__)
+
+    # set-up margules: Test inversion of "W(phl,obi)" & "W(phl,east)", within 0-60:
+    w_names =  ["W(phl,annm)",
+                "W(phl,obi)",
+                "W(phl,east)",
+                "W(phl,tbi)",
+                "W(phl,fbi)",
+                "W(phl,mmbi)",
+                "W(annm,obi)",
+                "W(annm,east)",
+                "W(annm,tbi)",
+                "W(annm,fbi)",
+                "W(annm,mmbi)",
+                "W(obi,east)",
+                "W(obi,tbi)",
+                "W(obi,fbi)",
+                "W(obi,mmbi)",
+                "W(east,tbi)",
+                "W(east,fbi)",
+                "W(east,mmbi)",
+                "W(tbi,fbi)",
+                "W(tbi,mmbi)",
+                "W(fbi,mmbi)"];
+
+    w_initial_values = [12   0 0;
+                        4    0 0;
+                        10   0 0;
+                        30   0 0;
+                        8    0 0;
+                        9    0 0;
+                        8    0 0;
+                        15   0 0;
+                        32   0 0;
+                        13.6 0 0;
+                        6.3  0 0;
+                        7    0 0;
+                        24   0 0;
+                        5.6  0 0;
+                        8.1  0 0;
+                        40   0 0;
+                        1    0 0;
+                        13   0 0;
+                        40   0 0;
+                        30   0 0;
+                        11.6 0 0];
+
+    w_lower_bounds =   [0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0];
+
+    w_upper_bounds =   [0 0 0;
+                        60 0 0;
+                        60 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0;
+                        0 0 0];
+
+    # load test data
+    path_mineral = "test_data/test_biotite_composition.csv";
+    path_bulk = "test_data/test_bulk.csv";
+    path_pt = "test_data/test_pt.csv";
+
+    constraints = load_constraints(path_bulk, path_mineral, path_pt, ["Si","Al","Mg", "Fe", "K", "Ti", "Mn"]);
+
+    # set-up two jobs for testing the two optimisation algorithms:
+    thermodynamic_database = "mp";
+    solid_solution = "bi";
+    number_constraints_max = 50;
+    number_iterations_max = 1000;
+    max_time_seconds = 600;
+
+    # 1. Nelder-Mead (with normalization)
+    algorithm = "NelderMead";
+    normalization = true;
+
+    JOB_NelderMead = job(thermodynamic_database, solid_solution, w_names, w_initial_values, w_lower_bounds, w_upper_bounds, algorithm, number_iterations_max, normalization, number_constraints_max, max_time_seconds);
+    job_check_consistency(JOB_NelderMead)
+
+    # 2. Particle Swarm (without normalization)
+    algorithm = "ParticleSwarm";
+    normalization = false;
+
+    JOB_ParticleSwarm = job(thermodynamic_database, solid_solution, w_names, w_initial_values, w_lower_bounds, w_upper_bounds, algorithm, number_iterations_max, normalization, number_constraints_max, max_time_seconds);
+    job_check_consistency(JOB_ParticleSwarm)
+
+    # RUN test inversions
+    res_NelderMead, norm_NelderMead = inversion_run(JOB_NelderMead, constraints)
+    res_ParticleSwarm, norm_ParticleSwarm = inversion_run(JOB_ParticleSwarm, constraints)
+
+    # check inverterted Margules parameters
+    @test res_NelderMead.minimizer .* norm_NelderMead ≈ [1.9729738468419615, 19.4110346874852]             atol=1e-6
+    @test res_ParticleSwarm.minimizer .* norm_ParticleSwarm ≈ [2.458257183285802, 19.862636192733326]      atol=1e-6
+end
+
+##############################################################################################################
+# Test of individual modules and functions therein
+#
+# Note: The tests are not exhaustive and do not cover all possible cases.
+#
+##############################################################################################################
+
 @testset "Bingo" begin
 
     obs_comp = [1,1.2,3.1]

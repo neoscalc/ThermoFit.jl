776770ff8863801b7e15b05c86c1fa300af56dd9
Philipsite <philip.hartmeier@hotmail.com>
2025-02-12 17:52:49 +0100

    Moved calculate_g0_corr our of forward call This should reduce the future duplications when implementing a multi phase forward call.

--- src/forward.jl
+++ src/forward.jl
@@ -47,20 +47,51 @@ function forward_call(phase, database, constraint, gv, z_b, DB, splx_data; w_g =
     end
 
     if !isnothing(g0_corr_vec) && !isnothing(g0_corr_em)
-        ss_gbase = unsafe_wrap(Vector{Float64}, ss_struct[ss_idx].gbase, ss_struct[ss_idx].n_em)
-        ss_emlist = unsafe_wrap(Vector{Ptr{Int8}}, ss_struct[ss_idx].EM_list, ss_struct[ss_idx].n_em)
-        ss_emlist = unsafe_string.(ss_emlist)
-        ss_gbase_mod = copy(ss_gbase)
-        for (em, g0_corr) in zip(g0_corr_em, g0_corr_vec)
-            em_idx = findfirst(ss_emlist .== em)
-            ss_gbase_mod[em_idx] += g0_corr
-        end
+        ss_gbase_mod = calculate_g0_corr(g0_corr_vec, g0_corr_em, ss_struct, ss_idx)
         unsafe_copyto!(ss_struct[ss_idx].gbase, pointer(ss_gbase_mod), ss_struct[ss_idx].n_em)
     end
-        
 
     out = pwm_run(gv, z_b, DB, splx_data)
     return out
+end
+
+
+function forward_call_multi_phase(phases            ::AbstractArray,
+                                  database          ::AbstractString,
+                                  constraint        ::Constraint,
+                                  gv, z_b, DB, splx_data;
+                                  phases_w_g        ::Union{AbstractArray,Nothing} = nothing,
+                                  phases_g0_corr_vec::Union{AbstractArray,Nothing} = nothing,
+                                  w_g               ::Union{AbstractArray,Nothing} = nothing,
+                                  g0_corr_vec       ::Union{AbstractArray,Nothing} = nothing,
+                                  g0_corr_em        ::Union{AbstractArray,Nothing} = nothing)
+
+    # check in the pahse list if for the specific mineral a W or G0 should be altered in the call
+    # then essentially move into the forward_call function
+
+end
+
+"""
+    calculate_g0_corr(g0_corr_vec, g0_corr_em, ss_struct, ss_idx)
+
+Adds a "correction" to the G0 values of endmembers in a solid solution.
+
+## Arguments
+- `g0_corr_vec`: A vector of 'n' corrections to be added on to G0 values of the endmembers.
+- `g0_corr_em`: A corresponding vector of 'n' endmembers to which the corrections are applied.
+- `ss_struct`: The solid solution structure from MAGEMin containing the endmembers.
+- `ss_idx`: The index of the solid solution in the structure.
+"""
+function calculate_g0_corr(g0_corr_vec, g0_corr_em, ss_struct, ss_idx)
+    ss_gbase = unsafe_wrap(Vector{Float64}, ss_struct[ss_idx].gbase, ss_struct[ss_idx].n_em)
+    ss_emlist = unsafe_wrap(Vector{Ptr{Int8}}, ss_struct[ss_idx].EM_list, ss_struct[ss_idx].n_em)
+    ss_emlist = unsafe_string.(ss_emlist)
+    ss_gbase_mod = copy(ss_gbase)
+    for (em, g0_corr) in zip(g0_corr_em, g0_corr_vec)
+        em_idx = findfirst(ss_emlist .== em)
+        ss_gbase_mod[em_idx] += g0_corr
+    end
+    return ss_gbase_mod
 end
 
 

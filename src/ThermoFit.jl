a3dca9f9ac4badeca1fc06d04db0f2aabc5f3a81
Philipsite <philip.hartmeier@hotmail.com>
2025-02-23 21:53:56 +0100

    Update pixelmap-like calculation to visualise PEM results - New module resplot.jl > pixelmap.jl - pixelmap() function, works with G_0 and w_g - add tests

--- src/ThermoFit.jl
+++ src/ThermoFit.jl
@@ -12,12 +12,12 @@ include("constraint.jl")
 include("forward.jl")
 include("inversion.jl")
 include("loss.jl")
-include("resplot.jl")
+include("pixelmap.jl")
 
 export Constraint, read_constraints_from_yml
 export forward_call, calculate_w_g
 export JOB, print_job, variable_optimised, inversion, objective_function
 export bingo_calculate_qcmp_phase, bingo_generate_fake_uncertainties, chi_squared
-export resplot, pixelmap_calc
+export pixelmap
 
 end

791753550d23bb860ba9d95775dd9082f892b31c
Philipsite <philip.hartmeier@hotmail.com>
2025-05-27 16:11:13 +0200

    Add plot_convergence()

--- src/ThermoFit.jl
+++ src/ThermoFit.jl
@@ -3,6 +3,7 @@ using Base.Threads
 using Random
 using Dates
 using Reexport: @reexport
+using Statistics
 using Optim
 using ProgressBars
 using OrderedCollections
@@ -20,6 +21,7 @@ include("inversion.jl")
 include("logging.jl")
 include("loss.jl")
 include("objective_f.jl")
+include("plot.jl")
 include("pixelmap.jl")
 
 export Constraint, read_constraints_from_yml, gen_constraints_for_functional_inv, read_FPWMP_bulks
@@ -28,6 +30,7 @@ export JOB, print_job, variable_optimised, inversion
 export print_job, print_constraints, print_results
 export objective_function, objective_function_func_relation
 export loss_Qfactor, quality_factor, bingo_generate_fake_uncertainties, chi_squared, Ti_in_Bt_misfit, Ti_sat_misfit
+export plot_convergence
 export pixelmap, pixmap_misfit_Ti_in_Bt_temperatures, pixmap_misfit_Ti_in_Bt_temperatures, pixmap_phase_comp_og_v_mod
 
 end
